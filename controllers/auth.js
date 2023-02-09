9662769c899af759a0e93d108033015e5ac53c09
kowany <hvilla65@gmail.com>
2023-02-09 15:01:27 -0600

    Fin sección 11 - version 4.0.0 - detección de url: localhost | railwayen el index.html

--- controllers/auth.js
+++ controllers/auth.js
@@ -100,52 +100,6 @@ const googleSignIn = async(req, res = response) => {
     }
 }
 
-// const googleSignIn = async (req, res = response) => {
-//     const { id_token } = req.body;
-//     try {
-//         const { correo, nombre, img } = await googleVerify(id_token);
-//         // console.log(correo, nombre, img)
-//         let usuario = await Usuario.findOne({ correo });
-//         if (!usuario) {
-//             // Tengo que crearlo
-//             const data = {
-//                 nombre,
-//                 correo,
-//                 password: ':P',
-//                 img,
-//                 google: true
-//             };
-//             console.log('data', data)            
-//             usuario = new Usuario(data);
-//             console.log('usuario', usuario)
-//             await usuario.save();
-//         }
-
-//         // Si el usuario en DB
-//         if (!usuario.estado) {
-//             return res.status(401).json({
-//                 msg: 'Hable con el administrador, usuario bloqueado'
-//             });
-//         }
-
-//         // Generar el JWT
-//         const token = await generarJWT(usuario.id);
-//         console.log('token', token)
-//         res.json({
-//             usuario,
-//             token
-//         });
-
-//     } catch (error) {
-
-//         res.status(400).json({
-//             msg: 'Token de Google no es válido'
-//         })
-
-//     }
-
-// }
-
 module.exports = {
     login,
     googleSignIn
